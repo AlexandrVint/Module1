0288325ef2b29dd1e8100cd6b38c396bca41cac2
AlexandrVint <alexandr.azizov@gmail.com>
2023-01-03 22:36:06 +0300

    Добавьте файлы проекта.

---
+++ Module1WPFConsoleTest/Module1WPFConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module1WPFConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
