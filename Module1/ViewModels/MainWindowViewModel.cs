d878f645eb25aedc0cf3a1fa777db4ddff9636ae
AlexandrVint <alexandr.azizov@gmail.com>
2023-01-03 22:53:39 +0300

    Создали MainWindowModel в папке ViewModel

---
+++ Module1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Module1.ViewModels.Base;
+
+namespace Module1.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+        #region Заголовок окна
+
+
+
+
+        #endregion
+
+        #region Статус программы
+
+        
+
+        #endregion
+
+        #region Конманды
+
+
+
+        #endregion
+
+        #region Свойства
+
+
+
+        #endregion
+
+        #region Конструктор MainViewModel
+
+
+
+        #endregion
+
+    }
+}
