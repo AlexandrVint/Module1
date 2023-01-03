a5fe1e7cbdf822a2fed28933353c208fbbdcc85e
AlexandrVint <alexandr.azizov@gmail.com>
2023-01-03 23:27:41 +0300

    Добавил заголовок к главному окну

--- Module1/ViewModels/MainWindowViewModel.cs
+++ Module1/ViewModels/MainWindowViewModel.cs
@@ -1,23 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Markup;
 using Module1.ViewModels.Base;
 
 namespace Module1.ViewModels
 {
+    [MarkupExtensionReturnType(typeof(MainWindowViewModel))]
     internal class MainWindowViewModel : ViewModel
     {
+        
 
         #region Заголовок окна
 
+        private string _Title = "Расчет подпора воздуха в лифтовую шахту";
 
+        public string Title
+        {
+            get => _Title;
+
+            set => Set(ref _Title, value);
+        }
 
 
         #endregion
 
         #region Статус программы
 
-        
+
 
         #endregion
 
