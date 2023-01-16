d5c3a093f3cabc3e9f2b4fd6e0b4638be9231099
AlexandrVint <alexandr.azizov@gmail.com>
2023-01-16 22:04:32 +0300

    Сделал интерфейс основныз трех окон

---
+++ Module1/Views/Window/TableFloorWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Module1.Views.Window
+{
+    /// <summary>
+    /// Логика взаимодействия для TableFloorWindow.xaml
+    /// </summary>
+    public partial class TableFloorWindow : System.Windows.Window
+    {
+        public TableFloorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
