57516f9e5572f5484c7bc62cc45eae5f62d1ad21
AlexandrVint <alexandr.azizov@gmail.com>
2023-02-19 02:42:40 +0300

    Добавил все классы для таблицы

---
+++ Module1/Models/AirFlow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module1.ViewModels.Base;
+
+namespace Module1.Models
+{
+    public class AirFlow:ViewModel
+    {
+        #region FlowG : double - массовый расход воздуха на этаже
+        private double _FlowG;
+        /// <summary>
+        /// массовый расход воздуха на этаже
+        /// </summary>
+        public double FlowG
+        {
+            get => _FlowG;
+            set => Set(ref _FlowG, value);
+        }
+        #endregion
+
+        #region AirFlowL : double - объемный расход воздуха на этаже
+        private double _AirFlowL;
+        /// <summary>
+        /// объемный расход воздуха на этаже
+        /// </summary>
+        public double AirFlowL
+        {
+            get => _AirFlowL;
+            set => Set(ref _AirFlowL, value);
+        }
+        #endregion
+
+    }
+}
---
+++ Module1/Models/AirFlowDelta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module1.ViewModels.Base;
+
+namespace Module1.Models
+{
+    public class AirFlowDelta:ViewModel
+    {
+        #region FlowDeltaDoorG : double - инфильтрация массового расхода воздуха через двери лифтовой шахты на жтаже
+        private double _FlowDeltaDoorG;
+        /// <summary>
+        /// инфильтрация воздуха 
+        /// </summary>
+        public double FlowDeltaDoorG
+        {
+            get => _FlowDeltaDoorG;
+            set => Set(ref _FlowDeltaDoorG, value);
+        }
+        #endregion
+
+    }
+}
