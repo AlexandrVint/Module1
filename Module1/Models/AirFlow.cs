08b89e23101796c1adec664a109474033189ec20
AlexandrVint <alexandr.azizov@gmail.com>
2023-02-19 11:20:29 +0300

    Поменял все свойства

--- Module1/Models/AirFlow.cs
+++ Module1/Models/AirFlow.cs
@@ -3,34 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Module1.ViewModels.Base;
 
 namespace Module1.Models
 {
     public class AirFlow:ViewModel
     {
-        #region FlowG : double - массовый расход воздуха на этаже
-        private double _FlowG;
+        
+
+        #region FlowG : double - массовый расход воздуха на этаже   
         /// <summary>
-        /// массовый расход воздуха на этаже
+        /// COMMENTS
         /// </summary>
-        public double FlowG
-        {
-            get => _FlowG;
-            set => Set(ref _FlowG, value);
-        }
+        public double FlowG { get; set; }
         #endregion
 
-        #region AirFlowL : double - объемный расход воздуха на этаже
-        private double _AirFlowL;
+        #region AirFlowL : double - объемный расход воздуха на этаже   
         /// <summary>
         /// объемный расход воздуха на этаже
         /// </summary>
-        public double AirFlowL
-        {
-            get => _AirFlowL;
-            set => Set(ref _AirFlowL, value);
-        }
+        public double AirFlowL { get; set; }
         #endregion
 
     }
